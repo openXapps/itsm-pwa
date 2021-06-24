1c35cff6f08b908403ed71820d4c9e84c5a90f44
OpenApps <32998735+openXapps@users.noreply.github.com>
2021-06-24 12:20:20 +0200

    Refined header functionality

--- src/components/Header/HeaderComponent.js
+++ src/components/Header/HeaderComponent.js
@@ -2,20 +2,13 @@ import { useState, useContext } from 'react';
 import { withRouter } from 'react-router-dom';
 
 import AppBar from '@material-ui/core/AppBar';
-import Drawer from '@material-ui/core/Drawer';
 import Toolbar from '@material-ui/core/Toolbar';
 import Container from '@material-ui/core/Container';
 import Typography from '@material-ui/core/Typography';
-import Box from '@material-ui/core/Box';
-import Divider from '@material-ui/core/Divider';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
-import Button from '@material-ui/core/Button';
+import MenuItem from '@material-ui/core/MenuItem';
+import Menu from '@material-ui/core/Menu';
 import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
-import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import AccountCircle from '@material-ui/icons/AccountCircle';
 import SettingsIcon from '@material-ui/icons/Settings';
 import Hidden from '@material-ui/core/Hidden';
 import Snackbar from '@material-ui/core/Snackbar';
@@ -26,22 +19,41 @@ import { context } from '../../context/StoreProvider';
 import { application } from '../../utilities/defaultdata';
 import { getLocalSession } from '../../utilities/localstorage';
 import { logout } from '../../service/AuthService';
+import { useEffect } from 'react';
 
 const HeaderComponent = ({ history, location }) => {
   const [state, dispatch] = useContext(context);
   const classes = useStyles();
-  const [drawerState, setDrawerState] = useState(false);
+  const user = getLocalSession().data.user;
+  const [userName, setUserName] = useState('');
+  const [anchorEl, setAnchorEl] = useState(null);
+  const menuOpen = Boolean(anchorEl);
   const [snackState, setSnackState] = useState({
     severity: 'success',
     message: 'Logout successful',
     show: false
   });
 
+  useEffect(() => {
+    if (user) {
+      const fullName = user.slice(0, user.indexOf('@'));
+      const firstName = fullName.slice(0, fullName.indexOf('.')).toUpperCase();
+      const lastName = fullName.slice(fullName.indexOf('.') + 1, 100).toUpperCase();
+      // console.log('HeaderComponent: fullName.,..', fullName);
+      // console.log('HeaderComponent: firstName...', firstName);
+      // console.log('HeaderComponent: lastName....', lastName);
+      setUserName(firstName + ' ' + lastName);
+    }
+    return () => { };
+  }, [user]);
+
   // Button handlers
-  const handleLoginButton = () => {
+  const handleLoginMenu = () => {
+    handleMenuClose();
     history.push('/login');
   };
-  const handleLogoutButton = () => {
+  const handleLogoutMenu = () => {
+    handleMenuClose();
     logout(false, getLocalSession().data.jwt);
     dispatch({ type: 'AUTH', payload: false });
     setSnackState({ ...snackState, show: true });
@@ -49,10 +61,13 @@ const HeaderComponent = ({ history, location }) => {
   const handleSettingsButton = () => {
     history.push('/settings');
   };
+  const handleMenuButton = (e) => {
+    setAnchorEl(e.currentTarget);
+  };
 
   // State handlers
-  const handleDrawerState = () => {
-    setDrawerState(!drawerState);
+  const handleMenuClose = () => {
+    setAnchorEl(null);
   };
   const handleSnackState = () => {
     setSnackState({ ...snackState, show: false });
@@ -68,35 +83,44 @@ const HeaderComponent = ({ history, location }) => {
             >Standard Bank ITSM <span className={classes.appVersion}><Hidden xsDown>v{application.version}</Hidden></span>
             </Typography>
             {location.pathname === '/' ? (
-              <>
-                {state.isAuth ? (
-                  <Button color="inherit" onClick={handleLogoutButton}>Logout</Button>
-                ) : (
-                  <Button color="inherit" onClick={handleLoginButton} >Login</Button>
-                )}
-                <IconButton className={classes.menuButton} color="inherit" aria-label="menu" onClick={handleDrawerState}>
-                  <MenuIcon />
+              <div>
+                <IconButton
+                  aria-label="account of current user"
+                  aria-controls="menu-appbar"
+                  aria-haspopup="true"
+                  onClick={handleMenuButton}
+                  color={state.isAuth ? 'primary' : 'inherit'}
+                ><AccountCircle /></IconButton>
+                <Menu
+                  id="menu-appbar"
+                  anchorEl={anchorEl}
+                  anchorOrigin={{
+                    vertical: 'top',
+                    horizontal: 'right',
+                  }}
+                  keepMounted
+                  transformOrigin={{
+                    vertical: 'top',
+                    horizontal: 'right',
+                  }}
+                  open={menuOpen}
+                  onClose={handleMenuClose}
+                >
+                  {state.isAuth ? (
+                    <MenuItem color="inherit" onClick={handleLogoutMenu}>Logout</MenuItem>
+                  ) : (
+                    <MenuItem color="inherit" onClick={handleLoginMenu} >Login</MenuItem>
+                  )}
+                  <MenuItem onClick={handleMenuClose} disabled={!state.isAuth}>{userName}</MenuItem>
+                </Menu>
+                <IconButton className={classes.menuButton} onClick={handleSettingsButton}>
+                  <SettingsIcon />
                 </IconButton>
-              </>
+              </div>
             ) : (null)}
           </Toolbar>
         </Container>
       </AppBar>
-      {/* <Toolbar /> */}
-      <Drawer anchor="right" open={drawerState} onClose={handleDrawerState}>
-        <div className={classes.drawerSize} onClick={handleDrawerState}>
-          <Box p={1}>
-            <Button onClick={handleDrawerState} fullWidth><ChevronRightIcon /></Button>
-          </Box>
-          <Divider />
-          <List>
-            <ListItem button onClick={handleSettingsButton} disabled={!state.isAuth}>
-              <ListItemIcon><SettingsIcon /></ListItemIcon>
-              <ListItemText primary="Settings" />
-            </ListItem>
-          </List>
-        </div>
-      </Drawer>
       <Snackbar
         anchorOrigin={{ vertical: 'bottom', horizontal: 'center', }}
         open={snackState.show}
